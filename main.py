d935bdde90e68bb68356904609878c7029ee5a7a
Yiğit Ali Tunç <yali52959gmail.com>
2023-10-27 11:50:26 +0300

    notları ekledim

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-# Ben İBB Teknoloji atölyelerine gidiyorum. Orada çok başarılıyım. bize yazdırdıkları bir kodu sana gostermek istiyorum.
-print("hello world")
+# Ben İBB Teknoloji atölyelerine gidiyorum. Orada çok başarılıyım. https://drive.google.com/file/d/1GXn0Ff7uqwY_VT6CsIxYI-uZRbqqhYi8/view?usp=sharing (kanıt) 
+#bize yazdırdıkları bir kodu sana gostermek istiyorum.
+
+#Bir şirket çalışanlarına mevcut maaaşlarının aralığına karşılık gelen değerler oranında zam yapacak. Örneğin;
+#Maaş 11.500 ile 15.000 arasında ise %15
+#Maaş 15.000 ile 20.000 arasında ise %20
+#Maaş 20.000 ile 25.000 arasında ise %25 zam yapılacaktır.
+#Not: bir çalışanın alabileceği minimum mmaş asgari ücrettir(11.500) Ve şirket bu sene 25.000'den fazla para alan çalışanlarına zam yapmayacaktır.
+#Kullanıcı bu bilgilere göre yanlış değer girerse onu uyarın*
